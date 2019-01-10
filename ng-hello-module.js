bdb51f2edd1d39811046470283b645fbc732ab60
freewind <nowindlee@gmail.com>
2019-01-10 22:10:23 +0800

    .

--- ng-hello-module.js
+++ ng-hello-module.js
@@ -3,19 +3,13 @@ import angular from 'angular';
 const ngHelloModule = angular.module('helloModule', []);
 
 ngHelloModule.directive('helloDirective', function () {
-    return {
-        scope: {
-            message: '=',
-            onMessageChange: '='
-        },
-        controller: function ($scope) {
-            $scope.message = 'Hello';
-            $scope.$watch('message', (message) => {
-                $scope.onMessageChange(message);
-            })
-        },
-        template: `<div>Angular: <input type="text" ng-model="message"></div>`
-    }
+  return {
+    scope: {
+      message: '=',
+      onMessageChange: '='
+    },
+    template: `<div>Angular: <input type="text" ng-model="message" ng-change="onMessageChange(message)"></div>`
+  }
 });
 
 export default ngHelloModule;
